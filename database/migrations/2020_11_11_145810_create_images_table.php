e8d004492bb2523e24866a2bc584d496d73f217b
Guido <guido.amodio@gmail.com>
2020-11-11 21:44:10 +0100

    table e model  img,

---
+++ database/migrations/2020_11_11_145810_create_images_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateImagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('images', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('apartment_id'); 
+            $table->foreign('apartment_id')->references('id')->on('apartments')->onDelete('cascade')->onUpdate('cascade'); 
+            $table->string('imgurl');
+            $table->boolean('cover');
+            $table->dateTime('created_at', 0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('images');
+    }
+}
