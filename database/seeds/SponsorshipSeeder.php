bca88c6669badc1925cdbb649f71f3881a42dd81
Guido <guido.amodio@gmail.com>
2020-11-12 21:39:21 +0100

    Fix-seeders-sponsorship

--- database/seeds/SponsorshipSeeder.php
+++ database/seeds/SponsorshipSeeder.php
@@ -13,11 +13,29 @@ class SponsorshipSeeder extends Seeder
      */
     public function run(Faker $faker)
     {
-        for ($i=0; $i <= 2 ; $i++) {
+        $price = [
+            2.99,
+            5.99,
+            9.99
+        ];
+
+        $time = [
+            24,
+            72,
+            144
+        ];
+
+        $plan = [
+            'silver',
+            'gold',
+            'platinum'
+        ];
+
+        for ($i=0; $i < count($price); $i++) {
             $sponsor = new Sponsorship;
-            $sponsor->name = $faker->colorName();
-            $sponsor->price = $faker->randomFloat($nbMaxDecimals = 2, $min = 0, $max = 100);
-            $sponsor->time = $faker->numberBetween($min = 0, $max = 124);
+            $sponsor->name = $plan[$i];
+            $sponsor->price = $price[$i];
+            $sponsor->time = $time[$i];
             $sponsor->save();
         }
     }
