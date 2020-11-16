2b87977966697a46827df9dfacf413585a291eae
Vincent <23vincent86@gmail.com>
2020-11-16 12:10:48 +0100

    fix

--- resources/views/show-apartment.blade.php
+++ resources/views/show-apartment.blade.php
@@ -15,12 +15,12 @@
         <!-- section titolo MOCKUP3 -->
 
         <div class="container">
-                <div class="card mb-3" data-id="{{$apartment->id}}">
-                <img src="https://a0.muscache.com/im/pictures/a521cb47-fe9b-4d6b-b69d-6777b012edf2.jpg?aki_policy=large" class="card-img-top" alt="...">
+            <div class="card mb-3">
+                <img class="card-img-top" alt="">
                 <div class="card-body">
                     <h5 class="card-title">Appartamento panoramico vista mare</h5>
-                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                    {{-- <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p> --}}
                 </div>
 
         </div>
@@ -56,8 +56,8 @@
 
             <div class="container py-5">
                 <div class="row">
-                    <div class="col-lg-6 wow animate__animated animate__flipInX animate__delay-1s">
-                        <img src="https://www.ladige.it/sites/www.ladige.it/files/styles/798x457/public/google%20maps.jpg?itok=G0_VMHYd" class="img-fluid" alt="">
+                    <div class="col-lg-6 wow animate__animated animate__flipInX animate__delay-1s" >
+                        <img src="https://www.ladige.it/sites/www.ladige.it/files/styles/798x457/public/google%20maps.jpg?itok=G0_VMHYd" class="img-fluid img-show" alt="">
                     </div>
 
 
