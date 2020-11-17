e8c53ab1efe92b09f30ce591c1d4a4ce96f126b6
Guido <guido.amodio@gmail.com>
2020-11-17 17:03:47 +0100

    commit sperem

--- resources/views/Host/dashboard.blade.php
+++ resources/views/Host/dashboard.blade.php
@@ -1,7 +1,12 @@
-@extends('layouts.extranet')        
+@extends('layouts.extranet')    
+
         
 @section('content')
-    <div class="box col-12 col-sm-6 col-md-8  col-lg-9 col-xl-9 d-flex">
+    <div class="box col-12 col-sm-6 col-md-8  col-lg-9 col-xl-9 pb-3">
+        <!-- appartamenti -->
+        <h2>My Appartments</h2>
+        <div class="apartments d-flex flex-wrap">
+        
         @foreach($apartments as $key=>$apartment)
             <div class="col-lg-3 mt-4 d-flex">
                 <div class="card card-e">
@@ -9,23 +14,45 @@
                         <h6 class="m-0 font-weight-bold text-primary">{{ $apartment->title }}</h6>
                     </div>
                     <img src="{{$cover[$key]->imgurl}}" class="card-img-top card-e-img-top" alt="{{ $apartment->title }}">
-                    <div class="card-body">
-                        
+                    <div class="card-body d-flex flex-column">
+                        <small>Numero stanze {{$apartment->n_rooms}}</small>
+                        <small>Numero letti {{$apartment->n_beds}}</small>
+                        <small>Numero bagni {{$apartment->n_bathrooms}}</small>
                     </div>
-                    <a class="btn btn-primary" href="{{route('apartments.edit',$apartment->id)}}">MODIFICA</a>
-                    <form action="{{route('apartments.destroy',$apartment->id)}}" method="post">
-                        @csrf
-                        @method('DELETE')
-                        <button type="submit" class="btn btn-danger">ELIMINA</button>
-                    </form>
                 </div>
             </div>
         @endforeach
+        </div>
+
+
+
+
+
+
     </div>
+  
+
+
+    <div class="chart col-12 col-sm-6 col-md-4 col-lg-3 ">
+        <!-- visualizzazioni -->
+        <canvas id="myChart"></canvas>
+        <hr>
+        <canvas id="myChart2"></canvas>
+        <hr>
+    </div>
+    
+    
+  
 
 
 
 
+@endsection
 
 
+@section('script')
+
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js" integrity="sha512-bLT0Qm9VnAYZDflyKcBaQ2gg0hSYNQrJ8RilYldYQ1FxQYoCLtUjuuRuZo+fjqhx/qtq/1itJ0C2ejDxltZVFg==" crossorigin="anonymous"></script>
+<script src="{{ asset('js/dashboard.js') }}"></script>
 @endsection
