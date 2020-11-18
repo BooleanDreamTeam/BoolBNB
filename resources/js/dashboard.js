9c98147f8b4f16b06b9e442bbeffc070a242b34d
Jona M <jonameleqi@gmail.com>
2020-11-18 16:14:15 +0100

    ok

--- resources/js/dashboard.js
+++ resources/js/dashboard.js
@@ -5,78 +5,85 @@ $(document).ready(function() {
     
     var ctx = document.getElementById('myChart').getContext('2d');
 var myChart = new Chart(ctx, {
-    type: 'bar',
+    type: "bar",
     data: {
-        labels: ['App1', 'App2', 'App3', 'App4'],
-        datasets: [{
-            label: 'Visualizzazioni del mese',
-            data: [12, 19, 3, 5,],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                // 'rgba(153, 102, 255, 0.2)',
-                // 'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                // 'rgba(153, 102, 255, 1)',
-                // 'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
+        labels: ["App1", "App2", "App3", "App4"],
+        datasets: [
+            {
+                label: "Visualizzazioni del mese",
+                data: [12, 19, 3, 5],
+                backgroundColor: [
+                    "rgba(255, 99, 132, 0.2)",
+                    "rgba(54, 162, 235, 0.2)",
+                    "rgba(255, 206, 86, 0.2)",
+                    "rgba(75, 192, 192, 0.2)"
+                    // 'rgba(153, 102, 255, 0.2)',
+                    // 'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    "rgba(255, 99, 132, 1)",
+                    "rgba(54, 162, 235, 1)",
+                    "rgba(255, 206, 86, 1)",
+                    "rgba(75, 192, 192, 1)"
+                    // 'rgba(153, 102, 255, 1)',
+                    // 'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }
+        ]
     },
     options: {
         scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
+            yAxes: [
+                {
+                    ticks: {
+                        beginAtZero: true
+                    }
                 }
-            }]
+            ]
         }
     }
 });
 
-
 // torta
-var ctx = document.getElementById('myChart2').getContext('2d');
+var ctx = document.getElementById("myChart2").getContext("2d");
 var myChart = new Chart(ctx, {
-    type: 'pie',
+    type: "pie",
     data: {
-        labels: ['Firefox', 'Chrome', 'Opera', 'Safari'],
-        datasets: [{
-            label: 'Visualizzazioni del mese',
-            data: [12, 19, 3, 5,],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                // 'rgba(153, 102, 255, 0.2)',
-                // 'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                // 'rgba(153, 102, 255, 1)',
-                // 'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
+        labels: ["Firefox", "Chrome", "Opera", "Safari"],
+        datasets: [
+            {
+                label: "Visualizzazioni del mese",
+                data: [12, 19, 3, 5],
+                backgroundColor: [
+                    "rgba(255, 99, 132, 0.2)",
+                    "rgba(54, 162, 235, 0.2)",
+                    "rgba(255, 206, 86, 0.2)",
+                    "rgba(75, 192, 192, 0.2)"
+                    // 'rgba(153, 102, 255, 0.2)',
+                    // 'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    "rgba(255, 99, 132, 1)",
+                    "rgba(54, 162, 235, 1)",
+                    "rgba(255, 206, 86, 1)",
+                    "rgba(75, 192, 192, 1)"
+                    // 'rgba(153, 102, 255, 1)',
+                    // 'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }
+        ]
     },
     options: {
         scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
+            yAxes: [
+                {
+                    ticks: {
+                        beginAtZero: true
+                    }
                 }
-            }]
+            ]
         }
     }
 });
