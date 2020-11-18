dd3a88b5988b544ebd58ad8bb46eebb011718391
Vincent <23vincent86@gmail.com>
2020-11-18 12:04:21 +0100

    push con preghiera

--- resources/views/show-apartment.blade.php
+++ resources/views/show-apartment.blade.php
@@ -17,7 +17,7 @@
         <div class="container">
             <div class="card card_show mb-3" data-lat="{{$apartment->latitude}}"  data-lng="{{$apartment->longitude}}" data-id="{{$apartment->id}}">
                 @foreach ($cover as $img)
-                    <img class="card-img-top" src="{{$img->imgurl}}" alt="{{$img->imgurl}}">
+                    <img class="card-img-top img-height" src="{{$img->imgurl}}" alt="{{$img->imgurl}}">
                 @endforeach
                 <div class="card-body">
                     <h5 class="card-title">{{$apartment->title}}</h5>
@@ -31,7 +31,32 @@
             <div class="container py-5">
                 <div class="row">
                     <div class="col-lg-6 wow animate__animated animate__fadeInLeft">
-                        <img src="https://www.thespruce.com/thmb/_WJOc-34GLmc5QAzOR-3TXKumu8=/960x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/bed-and-fireplace-in-luxury-bedroom-748316169-37b1062605034b23ab6d193be9c58ef6.jpg" class="img-fluid" alt="">
+                        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+                              <ol class="carousel-indicators">
+                                <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                                <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                                <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                              </ol>
+                              <div class="carousel-inner">
+                                <div class="carousel-item active">
+                                  <img class="d-block w-100" src="https://www.thespruce.com/thmb/_WJOc-34GLmc5QAzOR-3TXKumu8=/960x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/bed-and-fireplace-in-luxury-bedroom-748316169-37b1062605034b23ab6d193be9c58ef6.jpg" alt="First slide">
+                                </div>
+                                <div class="carousel-item">
+                                  <img class="d-block w-100" src="https://www.thespruce.com/thmb/_WJOc-34GLmc5QAzOR-3TXKumu8=/960x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/bed-and-fireplace-in-luxury-bedroom-748316169-37b1062605034b23ab6d193be9c58ef6.jpg" alt="Second slide">
+                                </div>
+                                <div class="carousel-item">
+                                  <img class="d-block w-100" src="https://www.thespruce.com/thmb/_WJOc-34GLmc5QAzOR-3TXKumu8=/960x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/bed-and-fireplace-in-luxury-bedroom-748316169-37b1062605034b23ab6d193be9c58ef6.jpg" alt="Third slide">
+                                </div>
+                              </div>
+                              <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                <span class="sr-only">Previous</span>
+                              </a>
+                              <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                <span class="sr-only">Next</span>
+                              </a>
+                        </div>
                     </div>
                     <div class="col-lg-6">
                         <div class="details wow animate__animated animate__fadeInRight">
@@ -39,15 +64,21 @@
                             <p>{{$apartment->description}}</p>
                         </div>
 
+
+
                         <div class="detail_bedroom wow animate__animated animate__fadeInRight animate__delay-1s">
-                            <h5 class="pt-5 pl-3">Bedroom</h5>
-                            <p class="pl-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam</p>
+                            <ul class="list-group">
+                                <li class="list-group-item list-group-item-light"><i class="fas fa-bed"></i></li>
+                                <li class="list-group-item list-group-item-light"><i class="fas fa-wifi"></i></li>
+                                <li class="list-group-item list-group-item-light"><i class="fas fa-bed"></i></li>
+                                <li class="list-group-item list-group-item-light"><i class="fas fa-wifi"></i></li>
+                                <li class="list-group-item list-group-item-light"><i class="fas fa-bed"></i></li>
+                            </ul>
+                            {{-- <h5 class="pt-5 pl-3">Bedroom</h5>
+                            <p class="pl-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam</p> --}}
                         </div>
 
-                        <div class="detail_rooms wow animate__animated animate__fadeInRight animate__delay-2s">
-                            <h5 class="pt-5 pl-3">Bedroom</h5>
-                            <p class="pl-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam</p>
-                        </div>
+
 
                     </div>
                 </div>
