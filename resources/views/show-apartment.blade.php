b18b04b08a92bb4e9c16b66c99a2110888302c58
Vincent <23vincent86@gmail.com>
2020-11-23 17:15:40 +0100

    second-layout-show

--- resources/views/show-apartment.blade.php
+++ resources/views/show-apartment.blade.php
@@ -2,7 +2,7 @@
 @extends('layouts.app')
 
 @section('css')
-<link href="{{ asset('css/show.css') }}" rel="stylesheet"> 
+<link href="{{ asset('css/show.css') }}" rel="stylesheet">
 
 @endsection
 
@@ -11,7 +11,7 @@
 @endsection
 
 @section('js')
-<script src="{{ asset('js/click.js') }}"></script> 
+<script src="{{ asset('js/click.js') }}"></script>
 @endsection
 
 @section('content')
@@ -139,7 +139,7 @@
                                         <input type="text" name="email" id="form-email" class="form-control" required>
                                     @else
                                         <input type="text" name="email" id="form-email" value="{{Auth::user()->email}}" class="form-control" required>
-                                    @endguest    
+                                    @endguest
                                     <label for="form-email">Your email</label>
                                 </div>
 
@@ -158,4 +158,137 @@
                 </div>
             </div>
         </section>
+
+
+
+
+
+
+
+
+        <section id="up" class="container d-none d-md-flex">
+            <div class="ctn-first col-6 py-2 pl-2 pr-1">
+                <div class="img p-5">
+
+                </div>
+            </div>
+
+            <div class="cont-second col-6 d-flex flex-column pl-1 pt-2 pr-2 pb-2">
+                <div class="img-ret pb-1">
+                    <div class="img">
+
+                    </div>
+                </div>
+                <div class="img-qdr d-flex m-0">
+                    <div class="col-6 uno pt-1 px-0 pr-1">
+                        <div class="img">
+
+                        </div>
+                    </div>
+                    <div class="col-6 due pt-1 px-0 pl-1">
+                        <div class="img">
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section id="mobile" class="container d-block d-md-none">
+            <div id="carouselExampleControls" class="carousel slide p-2" data-ride="carousel">
+                  <div class="carousel-inner">
+                    <div class="carousel-item active">
+                      <img class="d-block w-100" src="https://q-xx.bstatic.com/xdata/images/hotel/840x460/248963502.jpg?k=309d99860c910fbf7844859b09feca1483f39c0c79a9906e65e994ab2c1daef1&o=" alt="First slide">
+                    </div>
+                    <div class="carousel-item">
+                      <img class="d-block w-100" src="https://cf.bstatic.com/images/hotel/max1024x768/161/161745727.jpg" alt="Second slide">
+                    </div>
+                    <div class="carousel-item">
+                      <img class="d-block w-100" src="https://q-xx.bstatic.com/xdata/images/hotel/840x460/248963502.jpg?k=309d99860c910fbf7844859b09feca1483f39c0c79a9906e65e994ab2c1daef1&o=" alt="Third slide">
+                    </div>
+                    <div class="carousel-item">
+                      <img class="d-block w-100" src="https://q-xx.bstatic.com/xdata/images/hotel/840x460/248963502.jpg?k=309d99860c910fbf7844859b09feca1483f39c0c79a9906e65e994ab2c1daef1&o=" alt="Fourth slide">
+                    </div>
+                  </div>
+                  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                  </a>
+                  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                  </a>
+            </div>
+
+        </section>
+
+
+
+        <section id="" class="container  d-md-flex flex-column">
+            <div class="col-lg-12 col-md-12 col-sm-12 p-2">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Descrione</h5>
+                        <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <h5 class="card-title">Servizi</h5>
+                        <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+
+                    </div>
+                </div>
+            </div>
+
+        </section>
+
+
+        <section id="" class="container  d-md-flex flex-column">
+
+            <div class="col-lg-12 col-md-12 col-sm-12 p-2">
+                <div class="jumbotron">
+                    <h2 class="display-4">MAPPA!</h2>
+                    <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+                    <hr class="my-4">
+                    <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+                    <p class="lead">
+                        <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+                    </p>
+                </div>
+            </div>
+        </section>
+
+        <section id="" class="container  d-md-flex">
+
+            
+                <div class="col-lg-6 col-md-6 col-sm-12 p-2">
+                    <div class="jumbotron">
+                        <h2 class="display-4">Write To Us!</h2>
+                        <label for="exampleInputEmail1">Email address</label>
+                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+                        <hr class="my-4">
+                        <label for="exampleFormControlTextarea1">Example textarea</label>
+                        <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                            <a class="btn btn-primary btn-lg" href="#" role="button">Submit</a>
+                        </p>
+                    </div>
+                </div>
+
+                <div class="col-lg-6 col-md-6 col-sm-12 p-2">
+                    <div class="jumbotron">
+                        <h2 class="display-4">Write To Us!</h2>
+                        <label for="exampleInputEmail1">Email address</label>
+                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+                        <hr class="my-4">
+                        <label for="exampleFormControlTextarea1">Example textarea</label>
+                        <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                            <a class="btn btn-primary btn-lg" href="#" role="button">Submit</a>
+                        </p>
+                    </div>
+                </div>
+        </section>
+
+
+
+
+
 @endsection
